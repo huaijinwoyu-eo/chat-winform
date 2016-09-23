8af3e9129573652b64f593a42a7d7c32875312dc
huaijinwoyu-eo <18730601288@163.com>
2016-09-23 15:55:10 +0800

    code add

--- js/chat.js
+++ js/chat.js
@@ -17,7 +17,64 @@ var ChatObjInfo = React.createClass({
     }
 });
 //聊天信息组件
-var ChatMessage = React.createElement({
+var ChatFormAndMessage = React.createClass({
+    getInitialState:function(){
+        return{
+            data:""
+        }
+    },
+    HandleSubmit:function(item){
+        var addItem = {};
+        addItem.id = this.state.data.length+2;
+        addItem.type="chat-my";
+        addItem.message=item;
+        var MessageData = this.state.data;
+        MessageData.push(addItem);
+        this.setState({
+            data:MessageData
+        })
+    },
+    render:function(){
+        return(
+            React.createElement("div",{},
+                React.createElement(ChatBox,{data:this.state.data}),
+                React.createElement(ChatInput,{SubmitMethod:this.HandleSubmit})
+            )
+        )
+    },
+    componentWillMount:function(){
+        $.ajax({
+            url:"message.json",
+            type:"GET",
+            dataType:"json",
+            success:function(data){
+                this.setState({
+                    data:data
+                })
+            }.bind(this),
+            error:function(error){
+                console.log(error)
+            }
+        })
+    }
+});
+var ChatBox = React.createClass({
+    componentDidUpdate:function(){
+        $(".chat-content").scrollTop($(".chat-content")[0].clientHeight)
+    },
+    render:function(){
+        var newDate =[];
+        for(var i in this.props.data){
+            newDate.push(
+                React.createElement(ChatMessage,{key:this.props.data[i].id,type:this.props.data[i].type,message:this.props.data[i].message})
+            )
+        }
+        return(
+            React.createElement("div",{className:"chat-content"},newDate)
+        )
+    }
+});
+var ChatMessage = React.createClass({
     render:function(){
         return(
             React.createElement("div",{className:"chat-item clearfix"},
@@ -28,8 +85,79 @@ var ChatMessage = React.createElement({
         )
     }
 });
+var ChatInput = React.createClass({
+    render:function(){
+        return(
+            React.createElement("div",{className:"chat-input"},
+                React.createElement(ChatFunctionItem),
+                React.createElement(ChatSubmit,{SubmitMethod:this.props.SubmitMethod})
+            )
+        )
+    }
+});
+var ChatFunctionItem = React.createClass({
+    render:function(){
+        return(
+            React.createElement("div",{className:"clearfix"},
+                React.createElement("ul",{className:"fl"},
+                    React.createElement("li",{className:"function-item w20"},
+                        React.createElement("span",{className:"icon-font"})
+                    ),
+                    React.createElement("li",{className:"function-item w20"},
+                        React.createElement("span",{className:"icon-face"})
+                    ),
+                    React.createElement("li",{className:"function-item w20"},
+                        React.createElement("span",{className:"icon-picture"})
+                    )
+                ),
+                React.createElement("div",{className:"fr message-log w20"},
+                    React.createElement("span",{className:"icon-time"}),
+                    React.createElement("a",{href:"#"},"消息记录"),
+                    React.createElement("span",{className:"icon-caret-down"})
+                )
+            )
+        )
+    }
+});
+var ChatSubmit = React.createClass({
+    getInitialState:function(){
+        return{
+            text:""
+        }
+    },
+    HandleSubmit:function(event){
+        event.preventDefault();
+        this.props.SubmitMethod(this.state.text);
+        this.setState({
+            text:""
+        });
+        $.ajax({
+            url:"./messages.json",
+            type:"POST",
+            dataType:"json",
+            data:this.state.text
+        })
+    },
+    HandleChange:function(event){
+        this.setState({
+            text:event.target.value
+        })
+    },
+    render:function(){
+        return(
+            React.createElement("form",{className:"submit-content rel",onSubmit:this.HandleSubmit},
+                React.createElement("textarea",{className:"submit-input",value:this.state.text,onChange:this.HandleChange}),
+                React.createElement("button",{type:"submit",className:"submit-button abs"},"发送")
+            )
+        )
+    }
+});
 var href = window.location.search.slice(1).split("&");
 ReactDOM.render(
     React.createElement(ChatObjInfo,{imgUrl:href[1],name:decodeURI(href[2]),job:decodeURI(href[3])}),
     document.getElementById("chat-object")
+);
+ReactDOM.render(
+    React.createElement(ChatFormAndMessage,null),
+    document.getElementById("chat-content")
 );
